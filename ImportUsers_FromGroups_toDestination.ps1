b6491a22802415afce808f8e1465bcf8c0c01d20
ch-adams <100363330+ch-adams@users.noreply.github.com>
2025-02-27 16:54:37 +0000

    Add files via upload

---
+++ ImportUsers_FromGroups_toDestination.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module AzureAD
+Connect-AzureAD
+
+$GroupNames = @("Group1","Group2","Group3","Group4")
+$DestGroup = (Get-AzureADGroup -SearchString "EnterGroupTarget").objectID
+
+foreach($GroupName in $GroupNames){
+    $UserIDs = Get-AzureADGroupMember -ObjectId (Get-AzureADGroup -SearchString $GroupName -all $true).objectID|select -ExpandProperty objectID
+
+    ForEach($UserID in $UserIDs){
+        Try{
+            Add-AzureADGroupMember -ObjectId $DestGroup -RefObjectId $UserIDs
+        }
+        Catch{
+            Write-Host "User is already a member.."
+        }
+    }
+}
